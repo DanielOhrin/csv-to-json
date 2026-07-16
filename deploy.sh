a9e5a3229dd041629d55c9913df15dd9b20f9515
DanielOhrin <xqiam@outlook.com>
2026-07-16 12:38:49 -0500

    Created lambda role + policy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@ aws cloudformation deploy \
     --template-file ./test.yaml \
     --stack-name "CtJ-bucket" \
     --s3-prefix "CtJ" \
-    --no-execute-changeset
+    --no-execute-changeset \
+    --capabilities CAPABILITY_NAMED_IAM
+
+    # Need to create lambda, s3 bucket, configure lambda execution role?, and uhhm subscribe the lambda to the s3 putobject events...
